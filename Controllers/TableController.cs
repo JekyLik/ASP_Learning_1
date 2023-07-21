0c09b8a1f9d06f8f57fa213811db55535d52d79b
JekyLik <jekylik1989@gmail.com>
2023-07-21 23:55:15 +0300

    Create methods Update and Delete

--- Controllers/TableController.cs
+++ Controllers/TableController.cs
@@ -9,34 +9,17 @@ namespace ASP_Learning_1.Controllers;
 public class TableController : Controller
 {
     private readonly ILogger<TableController> _logger;
+    private static int _maxId = 0;
     private static readonly List<Product> _products = new()
         {
-            new Product() { Title = "книга", Description = "Война и Мир", Price = 500, Quantity = 2 },
-            new Product() { Title = "журнал", Description = "Мурзилка", Price = 300, Quantity = 5 },
-            new Product() { Title = "газета", Description = "Известия", Price = 50, Quantity = 25 },
-            //new Product("книга", "Мастер и Маргарита", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Анна Каренина", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Преступление и наказание", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Доктор Живаго", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Бесы", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Тихий Дон", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Мёртвые души", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Отцы и дети", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("книга", "Собачье сердце", Random.Next(100, 600), Random.Next(1, 10)),
-            //new Product("журнал", "Красота и здоровье", Random.Next(50, 300), Random.Next(1, 10)),
-            //new Product("журнал", "За рулем", Random.Next(50, 300), Random.Next(1, 10)),
-            //new Product("журнал", "Мурзилка", Random.Next(50, 300), Random.Next(1, 10)),
-            //new Product("журнал", "Вокруг света", Random.Next(50, 300), Random.Next(1, 10)),
-            //new Product("журнал", "Огонек", Random.Next(50, 300), Random.Next(1, 10)),
-            //new Product("газета", "Известия", Random.Next(50, 300), Random.Next(1, 10)),
-            //new Product("газета", "Комсомольская правда", Random.Next(10, 100), Random.Next(1, 10)),
-            //new Product("газета", "Российская газета", Random.Next(10, 100), Random.Next(1, 10)),
-            //new Product("газета", "Московский комсомолец", Random.Next(10, 100), Random.Next(1, 10)),
-            //new Product("газета", "Аргументы и Факты", Random.Next(10, 100), Random.Next(1, 10))
+            new Product() { Id = 1, Title = "книга", Description = "Война и Мир", Price = 500, Quantity = 2 },
+            new Product() { Id = 2, Title = "журнал", Description = "Мурзилка", Price = 300, Quantity = 5 },
+            new Product() { Id = 3, Title = "газета", Description = "Известия", Price = 50, Quantity = 25 },
         };
-        
+    
     public TableController(ILogger<TableController> logger)
     {
+        _maxId = _maxId != 0? _maxId : _products.Select(product => product.Id).Prepend(0).Max();
         _logger = logger;
     }
 
@@ -44,15 +27,55 @@ public class TableController : Controller
     {
         var model = new IndexModel
         {
-            Products = _products
+            Products = _products,
         };
         return View(model);
     }
-
+    
     [HttpPost("create-product")]
     public IActionResult CreateProduct([FromForm] Product newProduct)
     {
+        newProduct.Id = ++_maxId;
         _products.Add(newProduct);
         return RedirectToAction("Table", "Table");
     }
+    
+    [HttpGet("update-product/{id}")]
+    public IActionResult UpdateProduct([FromRoute] int id)
+    {
+        foreach (var model in _products.Where(model => model.Id == id))
+        {
+            return View(model);
+        }
+
+        return RedirectToAction("Table", "Table");
+    }
+    
+    [HttpPost("update")]
+    public IActionResult Update([FromForm]Product product)
+    {
+        for (var i = 0; i < _products.Count; i++)
+        {
+            if (_products[i].Id == product.Id)
+            {
+                _products[i] = product;
+                break;
+            }
+        }
+
+        return RedirectToAction("Table", "Table");
+    }
+    
+    [HttpGet("delete/{id}")]
+    public IActionResult Delete([FromRoute] int id)
+    {
+        foreach (var product in _products.Where(product => product.Id == id))
+        {
+            _products.Remove(product);
+            break;
+        }
+
+        return RedirectToAction("Table", "Table");
+    }
+    
 }
